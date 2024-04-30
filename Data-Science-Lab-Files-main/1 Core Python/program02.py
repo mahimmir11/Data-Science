85a2cf3c72084d9fd6676bf61d2c85a33e836172
arpanp001 <168514234+arpanp001@users.noreply.github.com>
2024-04-30 12:57:00 +0530

    Delete Data-Science-Lab-Files-main directory

--- Data-Science-Lab-Files-main/1 Core Python/program02.py
+++
@@ -1,3 +0,0 @@
-#2. split the string s = "hi there sam" into a list.
-c = "hi there sam"
-print(c.split())
--- Data-Science-Lab-Files-main/1 Core Python/program08.py
+++
@@ -1,5 +0,0 @@
-#8. Create a basic function that returns True if the word 'dog' is contained in the input string. Dont worry about the edge cases like a 
-## punctuation being attached to the word 'dog'.
-def find_dog(s):
-    return "dog" in s.lower()
-print(find_dog("Is there a dog here?"))
